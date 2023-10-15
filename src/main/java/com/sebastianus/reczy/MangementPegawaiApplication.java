34adaa6a47e5c111027a8d3ff0d5d1f22ed41119
sebastianusreczys <sebastianusreczy17@gmail.com>
2023-10-15 09:48:00 +0700

    setup project

---
+++ src/main/java/com/sebastianus/reczy/MangementPegawaiApplication.java
@@ -0,0 +1,13 @@
+package com.sebastianus.reczy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangementPegawaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangementPegawaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastianus/reczy/MangementPegawaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianus.reczy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangementPegawaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
